177988105fd4b0c9bea7d6e1bf55b0f801e3c3b6
Harrison Hall <harryson.hall@gmail.com>
2024-09-27 23:49:00 -0400

    baseline commit for privacy page

--- src/Pages/Privacy/Privacy.jsx
+++ src/Pages/Privacy/Privacy.jsx
@@ -1,14 +1,57 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-import './Privacy.css';
+// import './Privacy.css';
 
 
 const Privacy = () => {
 
-	return (
-		<div id='privacy'>
+	useEffect(() => {
+		// onStart Animation
+		// document.querySelector('#privacy-main').childNodes.forEach((child, index) => {
 
-			<div id='privacy-main-container'>
+		// 	child.style.filter = 'blur(10px)';
+		// 	child.style.opacity = '0';
+			
+		// 	child.style.animation = `onStartBlur 0.5s ease-in-out forwards`;
+		// 	child.style.animationDelay = `${index * 0.075}s`;
+		// });
+
+		// document.querySelector('#privacy-page').style.animationDelay = '0.5s'
+		// document.querySelector('#privacy-page').style.animation = `onStartBlur 3s ease-in-out forwards`
+	}, [])
+
+	return (
+		<div id='privacy-page'>
+
+
+			<div id='privacy-main'>
+
+				<div id='privacy-left-line'></div>
+				<div id='privacy-right-line'></div>
+
+				<div id="privacy-top">
+				</div>
+
+				<div id='privacy-top-line'></div>
+
+				<div id="privacy-middle">
+					Privacy
+				</div>
+
+				<div id='privacy-bottom-line'></div>
+
+				<div id="privacy-bottom">
+					Last Updated September 11 2024
+				</div>
+				
+			</div>
+
+			<div id='privacy-description'>
+			The privacy policy is simple:<br /><br /> Castr application does not collect any of your personal data, nor any data from anywhere for that matter. No personal data shared with us will be provided to any third parties under any circumstances without your explicit permission and consent.
+			Castr does not engage in any ad targeting, data mining, or other activities that may compromise your privacy, and Castr does not affiliate itself with any third parties that do so.
+			</div>
+
+			{/* <div id='privacy-main-container'>
 				
 				<div id='privacy-header'>
 					Privacy Policy
@@ -23,7 +66,7 @@ const Privacy = () => {
 					The app engages in no ad targeting, data mining, or other activities that may compromise your privacy, and we do not affiliate ourselves with any third parties that do so.
 				</div>
 
-			</div>
+			</div> */}
 
         </div>
 	)
